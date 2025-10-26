0a3381776cedec0dfc6432a45bdd3b13912d61dc
RUTHVIK A <138464621+ruthvik-172004@users.noreply.github.com>
2025-10-26 19:53:55 +0000

    Add tests for script1.js

--- script1.test.js
+++ script1.test.js
@@ -2,6 +2,6 @@
 const add = require('./script1.js');
 
 // This is the actual test
-test('Check if 1 + 2 equals 6', () => {
+test('Check if 1 + 2 equals 3', () => {
   expect(add(1, 2)).toBe(6);
 });
