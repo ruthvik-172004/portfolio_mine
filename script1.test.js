b0c4b42ed63d8b37c6b05d4c27078173e939de9d
RUTHVIK A <138464621+ruthvik-172004@users.noreply.github.com>
2025-10-26 19:46:38 +0000

    Add tests for script1.js

--- script1.test.js
+++ script1.test.js
@@ -1,5 +1,5 @@
 // This 'imports' the add function from your script.js
-const add = require('./script.js');
+const add = require('./script1.js');
 
 // This is the actual test
 test('Check if 1 + 2 equals 3', () => {
